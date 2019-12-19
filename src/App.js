065aa005dfbc6bf8e654517a4c7111f547434598
jayro <jayro1996.ag@gmail.com>
2019-12-18 23:00:03 -0500

    agregando header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    noticias: []
+  }
+
+  //aca se realiza el llamado a las apis 
+  //en el metodo de ciclo de vida componentDidMount()
+  componentDidMount() {
+    this.consultarNoticias();
+
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3a71880af2cd409cbc4e3d4fec8a39f2`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+
+
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header titulo="Noticias React API" />
+
+        <div className="container white contenedor-noticias"></div>
+      </Fragment>
+
+    );
+  }
 }
 
 export default App;
