0f0ad14ac977dc4a29953ed9843357ae785d839b
jayro <jayro1996.ag@gmail.com>
2019-12-19 21:37:24 -0500

    itinerando las noticias y llamando a los componentes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({ titulo }) => {
       return (
 
             <nav className="nav-wrapper light-blue darken-3">
-                  <a href="" className="brand-logo center">{titulo}</a>
+                  <a href="index.html" className="brand-logo center">{titulo}</a>
             </nav>
       );
 };
---
+++ src/components/Noticia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Noticia = ({ noticia }) => {
+      return (
+            <h1 className="">
+                  {noticia}
+            </h1>
+      );
+};
+
+export default Noticia;
