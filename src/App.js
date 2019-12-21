2af5c36de91658966fb6be44a6edce251b77036b
jayro <jayro1996.ag@gmail.com>
2019-12-20 23:12:49 -0500

    creando un buscador de categorias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
 import ListaNoticias from './components/ListaNoticias';
+import Formulario from './components/Formulario';
+
 
 class App extends Component {
 
@@ -34,6 +36,7 @@ class App extends Component {
         <Header titulo="Noticias React API" />
 
         <div className="container white contenedor-noticias">
+          <Formulario />
           <ListaNoticias noticias={this.state.noticias} />
         </div>
       </Fragment>
